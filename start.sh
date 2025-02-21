99ffb734104fc74a28ef22fa4c7d99c9aa6cb618
furio <139717584+furipaf@users.noreply.github.com>
2025-02-22 01:27:44 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# For Web View Mode
-python3 app.py
+# Activate virtual environment
+source /home/$(whoami)/social-counter/venv/bin/activate
+
+# Start Flask application
+python3 /home/$(whoami)/social-counter/app.py
 
 # For HDMI Display Mode (uncomment below)
 # chromium-browser --kiosk --incognito http://localhost:5000
